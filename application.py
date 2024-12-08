5191fa8ab42de1f9db96bcf2284f1634ec6355b8
Adi3042 <aditya30042002yadav@gmail.com>
2024-12-08 18:47:19 +0530

    Notebooks of training Model

--- application.py
+++ application.py
@@ -1,26 +1,53 @@
-from flask import Flask,request,render_template,jsonify
-from logging import log
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import base64
+from flask import Flask, render_template, request
 
-application = Flask(__name__)
-app = application
+app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def home_page():
-    try:
-        results = ""
-        if request.method=='GET':
-            return render_template('index.html')
-        
-        else:
-            file = request.files['file']
-            file.save(file.filename)
-            results = "File uploaded successfully"
-            return render_template('index.html',results=results)
-        
-    except Exception as e:
-        log.error("Error in home_page: "+str(e))
-        return render_template('index.html',results="Error in uploading file")
-    
+# Load the Keras model
+model = tf.keras.models.load_model('saved_models/Chest_Xray_Model.keras')
+
+class_names = ['NORMAL', 'PNEUMONIA']
+
+def predict(model, img):
+    img = img.resize((128, 128))  # Resize image to match model input size
+    img_array = np.expand_dims(np.array(img), axis=0).astype(np.float32) / 255.0
+
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * np.max(predictions[0]), 2)
+    return predicted_class, confidence
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            img = Image.open(BytesIO(file.read()))
+            predicted_class, confidence = predict(model, img)
+            img_data = BytesIO()
+            img.save(img_data, format='PNG')
+            img_data.seek(0)
+            img_base64 = base64.b64encode(img_data.getvalue()).decode('utf-8')
+            return render_template(
+                'index.html',
+                img_data=img_base64,
+                class_name=predicted_class,
+                confidence=confidence
+            )
+    # Render fresh page on GET requests
+    return render_template('index.html')
+
+@app.route('/contactUs')
+def contact_us():
+    return render_template('contactUs.html')
+
+@app.route('/aboutUs')
+def about_us():
+    return render_template('aboutus.html')
 
 if __name__ == '__main__':
-    application.run(host = "0.0.0.0", debug = True, port = 5000)
+    app.run(debug=True)
